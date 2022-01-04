a619a30dd12777fb52c9af2aa586d953b55300d5
shenxingchao <864318466@qq.com>
2022-01-04 12:01:56 +0800

    [add]1.添加拖动成功粒子特效，并解决了层级问题

--- assets/Scripts/sences/one_type.js
+++ assets/Scripts/sences/one_type.js
@@ -51,155 +51,171 @@ cc.Class({
         cc.JsonAsset,
         function (err, asset) {
           map_array = asset.json;
+          // 加载 星星粒子 Prefab
+          cc.resources.load("prefab/star", function (err, prefab) {
+            //星星粒子预制
+            let start_perfab = cc.instantiate(prefab);
+            //设置底图精灵贴图
+            _this.game_background.getComponent(cc.Sprite).spriteFrame =
+              res.find((item) => item.name == "background");
+            //根据name赋值给指定精灵数组的索引 不然打包后顺序错乱Bug
+            let sprite_array = [];
+            res.forEach((item) => {
+              if (item.name !== "background") {
+                sprite_array[parseInt(item.name)] = item;
+              }
+            });
 
-          //设置底图精灵贴图
-          _this.game_background.getComponent(cc.Sprite).spriteFrame = res.find(
-            (item) => item.name == "background"
-          );
-          //根据name赋值给指定精灵数组的索引 不然打包后顺序错乱Bug
-          let sprite_array = [];
-          res.forEach((item) => {
-            if (item.name !== "background") {
-              sprite_array[parseInt(item.name)] = item;
+            //循环添加阴影图,并设置每个节点的索引序号
+            for (let i = 0; i < map_array.length; i++) {
+              //创建节点  名称为黑色阴影图加索引
+              let node = new cc.Node("shadow" + i);
+              //设置开始不可见
+              node.active = false;
+              //设置序号
+              node.index = i;
+              //设置位置属性
+              node.setPosition(map_array[i].x, map_array[i].y);
+              //设置缩放属性
+              node.setScale(map_array[i].scale_x, map_array[i].scale_y);
+              //添加精灵组件使之成为精灵
+              let sprite = node.addComponent(cc.Sprite);
+              //设置精灵贴图
+              sprite.spriteFrame = sprite_array[i];
+              //设置混合模式 这样就变成黑图了
+              sprite.srcBlendFactor = cc.macro.BlendFactor.ZERO;
+              //添加到父节点里面
+              _this.game_background.addChild(node);
             }
-          });
 
-          //循环添加阴影图,并设置每个节点的索引序号
-          for (let i = 0; i < map_array.length; i++) {
-            //创建节点  名称为黑色阴影图加索引
-            let node = new cc.Node("shadow" + i);
-            //设置开始不可见
-            node.active = false;
-            //设置序号
-            node.index = i;
-            //设置位置属性
-            node.setPosition(map_array[i].x, map_array[i].y);
-            //设置缩放属性
-            node.setScale(map_array[i].scale_x, map_array[i].scale_y);
-            //添加精灵组件使之成为精灵
-            let sprite = node.addComponent(cc.Sprite);
-            //设置精灵贴图
-            sprite.spriteFrame = sprite_array[i];
-            //设置混合模式 这样就变成黑图了
-            sprite.srcBlendFactor = cc.macro.BlendFactor.ZERO;
-            //添加到父节点里面
-            _this.game_background.addChild(node);
-          }
+            //获取底部工具条的宽度和高度 设置默认拖动项的宽高
+            _this.tool_item_width = (_this.tool_bar_box.width - 2 * 10) / 3;
+            _this.tool_item_height = _this.tool_item_width;
+            //设置拖动项精灵
+            _this.drag_items = [];
+            for (let i = 0; i < map_array.length; i++) {
+              //创建节点  名称为原图加索引
+              let node = new cc.Node("source" + i);
+              //添加精灵组件使之成为精灵
+              let sprite = node.addComponent(cc.Sprite);
+              //设置精灵贴图
+              sprite.spriteFrame = sprite_array[i];
+              //设置为自定义尺寸
+              sprite.sizeMode = cc.Sprite.SizeMode.CUSTOM;
+              //获取原图大小
+              const { width: origin_width, height: origin_height } =
+                node.getContentSize();
+              //假设宽高最多为212 自适应图片宽高
+              if (sprite.width > sprite.height) {
+                node.width = 212;
+                node.height = (212 * origin_height) / origin_width;
+              } else {
+                node.width = (212 * origin_width) / origin_height;
+                node.height = 212;
+              }
+              //添加到拖动精灵组
+              _this.drag_items.push(node);
+              //给每个节点添加拖动事件  拖动时显示对应阴影图
 
-          //获取底部工具条的宽度和高度 设置默认拖动项的宽高
-          _this.tool_item_width = (_this.tool_bar_box.width - 2 * 10) / 3;
-          _this.tool_item_height = _this.tool_item_width;
-          //设置拖动项精灵
-          _this.drag_items = [];
-          for (let i = 0; i < map_array.length; i++) {
-            //创建节点  名称为原图加索引
-            let node = new cc.Node("source" + i);
-            //添加精灵组件使之成为精灵
-            let sprite = node.addComponent(cc.Sprite);
-            //设置精灵贴图
-            sprite.spriteFrame = sprite_array[i];
-            //设置为自定义尺寸
-            sprite.sizeMode = cc.Sprite.SizeMode.CUSTOM;
-            //获取原图大小
-            const { width: origin_width, height: origin_height } =
-              node.getContentSize();
-            //假设宽高最多为212 自适应图片宽高
-            if (sprite.width > sprite.height) {
-              node.width = 212;
-              node.height = (212 * origin_height) / origin_width;
-            } else {
-              node.width = (212 * origin_width) / origin_height;
-              node.height = 212;
-            }
-            //添加到拖动精灵组
-            _this.drag_items.push(node);
-            //给每个节点添加拖动事件  拖动时显示对应阴影图
-
-            //定义按下回调
-            let node_pos = null;
-            //阴影图
-            let shadow_node = _this.game_background.getChildByName(
-              "shadow" + i
-            );
-            //设置拖动成功标志
-            _this.drag_on_flag = false;
-            let onTouchDown = function (e) {
+              //定义按下回调
+              let node_pos = null;
+              //阴影图
+              let shadow_node = _this.game_background.getChildByName(
+                "shadow" + i
+              );
               //设置拖动成功标志
               _this.drag_on_flag = false;
-              //拖动开始，记录初始位置
-              node_pos = e.getLocation();
-              //设置其父节点为根节点
-              node.setParent(_this.node);
-              //设置尺寸模式为原图模式
-              sprite.sizeMode = cc.Sprite.SizeMode.RAW;
-              //设置其大小为阴影图缩放大小
-              node.setScale(shadow_node.scaleX, shadow_node.scaleY);
-              //显示阴影图
-              shadow_node.active = true;
-              //设置初始x,y
-              node.setPosition(
-                node.parent.convertToNodeSpaceAR(node_pos).x,
-                node.parent.convertToNodeSpaceAR(node_pos).y + 200
-              );
-            };
-
-            //定义拖动回调
-            let onTouchMove = function (e) {
-              let delta = e.getDelta();
-              node.x += delta.x;
-              node.y += delta.y;
-              //判断2个物体中心距离 都转换成世界坐标
-              let pos_source = node.parent.convertToWorldSpaceAR(node.position);
-              let pos_shadow = shadow_node.parent.convertToWorldSpaceAR(
-                shadow_node.position
-              );
-              let distance = Math.sqrt(
-                Math.abs(
-                  Math.pow(pos_source.y - pos_shadow.y, 2) +
-                    Math.pow(pos_source.x - pos_shadow.x, 2)
-                )
-              );
-              //算错了这个距离
-              if (distance < 20) {
-                //拖拽成功
-                _this.drag_on_flag = true;
-                //放置止到阴影位置
-                node.setParent(_this.game_background);
-                node.setPosition(shadow_node.position);
-                //生成拖动项
-                _this._initSpriteItem();
-              } else {
+              let onTouchDown = function (e) {
+                //设置拖动成功标志
                 _this.drag_on_flag = false;
-              }
-            };
+                //拖动开始，记录初始位置
+                node_pos = e.getLocation();
+                //设置其父节点为根节点
+                node.setParent(_this.node);
+                //设置尺寸模式为原图模式
+                sprite.sizeMode = cc.Sprite.SizeMode.RAW;
+                //设置其大小为阴影图缩放大小
+                node.setScale(shadow_node.scaleX, shadow_node.scaleY);
+                //显示阴影图
+                shadow_node.active = true;
+                //设置初始x,y
+                node.setPosition(
+                  node.parent.convertToNodeSpaceAR(node_pos).x,
+                  node.parent.convertToNodeSpaceAR(node_pos).y + 200
+                );
+              };
 
-            //定义松开回调
-            let onTouchUp = function () {
-              if (!_this.drag_on_flag) {
-                // 还原到原来的数组
-                node.setParent(_this.tool_bar_box);
-                node.setPosition(0, 0);
-                node.setScale(1, 1);
-              } else {
-                //播放叮音效
-                if (audio_manage) {
-                  audio_manage.playDingEffect();
-                  if (_this.tool_bar_box.children.length == 0) {
-                    //播放游戏结束，挑战成功音效
-                    audio_manage.playGameSuccessEffect();
-                  }
+              //定义拖动回调
+              let onTouchMove = function (e) {
+                let delta = e.getDelta();
+                node.x += delta.x;
+                node.y += delta.y;
+                //判断2个物体中心距离 都转换成世界坐标
+                let pos_source = node.parent.convertToWorldSpaceAR(
+                  node.position
+                );
+                let pos_shadow = shadow_node.parent.convertToWorldSpaceAR(
+                  shadow_node.position
+                );
+                let distance = Math.sqrt(
+                  Math.abs(
+                    Math.pow(pos_source.y - pos_shadow.y, 2) +
+                      Math.pow(pos_source.x - pos_shadow.x, 2)
+                  )
+                );
+                //算错了这个距离
+                if (distance < 20) {
+                  //拖拽成功
+                  _this.drag_on_flag = true;
+                  //放置止到阴影位置
+                  node.setParent(_this.game_background);
+                  node.setPosition(shadow_node.position);
+                  //生成拖动项
+                  _this._initSpriteItem();
+                } else {
+                  _this.drag_on_flag = false;
                 }
-              }
-              _this.game_background.getChildByName("shadow" + i).active = false;
-            };
+              };
 
-            node.on("touchstart", onTouchDown, this);
-            node.on("touchmove", onTouchMove, this);
-            node.on("touchend", onTouchUp, this);
-            node.on("touchcancel", onTouchUp, this);
-          }
-          //添加精灵到拖动工具条内部直到添加完或者拖动项为3
-          _this._initSpriteItem();
+              //定义松开回调
+              let onTouchUp = function () {
+                if (!_this.drag_on_flag) {
+                  // 还原到原来的数组
+                  node.setParent(_this.tool_bar_box);
+                  node.setPosition(0, 0);
+                  node.setScale(1, 1);
+                } else {
+                  //播放叮音效
+                  if (audio_manage) {
+                    audio_manage.playDingEffect();
+                    if (_this.tool_bar_box.children.length == 0) {
+                      //播放游戏结束，挑战成功音效
+                      audio_manage.playGameSuccessEffect();
+                    }
+                  }
+                  //播放粒子动画 设置层级防止挡住
+                  start_perfab.setSiblingIndex(101)
+                  start_perfab.x = node.x;
+                  start_perfab.y = node.y;
+                  start_perfab.setParent(_this.game_background);
+                  //播放粒子
+                  let ParticleSystem = start_perfab.getComponent(
+                    cc.ParticleSystem
+                  );
+                  //重置粒子系统(播放粒子)
+                  ParticleSystem.resetSystem();
+                }
+                shadow_node.active = false;
+              };
+
+              node.on("touchstart", onTouchDown, this);
+              node.on("touchmove", onTouchMove, this);
+              node.on("touchend", onTouchUp, this);
+              node.on("touchcancel", onTouchUp, this);
+            }
+            //添加精灵到拖动工具条内部直到添加完或者拖动项为3
+            _this._initSpriteItem();
+          });
         }
       );
     });
