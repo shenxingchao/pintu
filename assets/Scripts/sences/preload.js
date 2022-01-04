425eaeb3901c4103b5c4d8cedfba12932dcc26ad
shenxingchao <864318466@qq.com>
2022-01-04 10:16:44 +0800

    [bug fix]1.Loading改为loading需要小写

--- assets/Scripts/sences/preload.js
+++ assets/Scripts/sences/preload.js
@@ -49,7 +49,7 @@ cc.Class({
     if (
       !this.start_button.active &&
       this.play_type_preload_finished &&
-      this.loading_bg.getComponent("Loading").isLoadFinished()
+      this.loading_bg.getComponent("loading").isLoadFinished()
     ) {
       //隐藏进度条
       this.loading_bg.active = false;
