1946955612ffa4236abd1b79868e53dd9900c6b4
shenxingchao <864318466@qq.com>
2022-01-10 17:21:32 +0800

    [add]1.完成碰撞检测测试

--- assets/Scripts/sences/three_type.js
+++ assets/Scripts/sences/three_type.js
@@ -1,3 +1,5 @@
+import collider from "../components/collider";
+
 cc.Class({
   extends: cc.Component,
 
@@ -16,7 +18,15 @@ cc.Class({
     cc.director.getPhysicsManager().enabled = true;
     //设置重力加速度 下降640世界单位/秒
     cc.director.getPhysicsManager().gravity = cc.v2(0, -640);
-    //设置墙体的位置
+
+    // //测试碰撞事件 后面改成预制体之间的碰撞了
+    // cc.find("Canvas/background/1").on(
+    //   "onBeginContactEvent",
+    //   (selfCollider, otherCollider) => {
+    //     //判断对象类型 类型一样的进行合并 播放合并粒子动画 删除2个对象 生成新的对象
+    //     console.log(123);
+    //   }
+    // );
   },
 
   start() {},
